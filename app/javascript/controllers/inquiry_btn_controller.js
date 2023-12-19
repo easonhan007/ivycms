c47866a75ee44acab5708ef36712fad4f5a358b0
Yichun Han <yichun.han@shopee.com>
2023-12-19 17:05:42 +0800

    add seo support for category and product

---
+++ app/javascript/controllers/inquiry_btn_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="inquiry-btn"
+export default class extends Controller {
+  connect() {
+  }
+
+  to_form(e) {
+    e.preventDefault();
+    document.querySelector("#inquiry_email").focus();
+  }
+}
